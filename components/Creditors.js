d665ac4794ff1b74b077606842970f8104fadb42
Bilal Shabbir <bilalshabbir725@gmail.com>
2022-02-27 23:23:00 +0500

    creditors title conditional style updated.

--- components/Creditors.js
+++ components/Creditors.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const Creditors = () => {
+
+ const [isMobile, setIsMobile] = useState(window.innerWidth < 1200);
+
+ {
+   /* Performs similarly to componentDidMount in classes */
+ }
+ useEffect(() => {
+   window.addEventListener(
+     "resize",
+     () => {
+       const ismobile = window.innerWidth < 1200;
+       if (ismobile !== isMobile) setIsMobile(ismobile);
+     },
+     false
+   );
+ }, [isMobile]);
+
+  const className = isMobile ? "col" : "d-flex justify-content-center";
+
   const accreditBy = [
     "schuco.png",
     "rehau.png",
@@ -11,13 +30,12 @@ const Creditors = () => {
     "british-enestraltion.jpeg",
     "aluplast.png",
   ];
-
-
-
+  
+  console.log(className)
   return (
     <div className="center-block m-5">
-      <div className="d-flex justify-content-center col-md-12 col-sm-6">
-        <img className="img-responsive" src="assets/images/logo.jpeg" alt="" style={{ width: 250 }} />
+      <div className={className}>
+        <img className="img-responsive" src="assets/images/logo.jpeg" alt="" />
         <h1 className="text-muted">are accredited by...</h1>
       </div>
       <div className="col-xs-1" align="center">
